b11b706a00234d090fbb14af1a997930ff388f86
krakosky <rperez.dev@gmail.com>
2018-01-03 13:53:48 +0100

    3/1/2018

--- app/src/main/java/com/buttons/lacueva/krakosky/lacuevabuttons/MainActivity.java
+++ app/src/main/java/com/buttons/lacueva/krakosky/lacuevabuttons/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,20 +12,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtview_btnA;
     private ImageButton imgbtn_btnA;
 
+    private LinearLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SoundButton sbutton = new SoundButton(
+                MainActivity.this,
+                "Sample",
+                R.raw.sample,
+                SoundButton.Color.BLUE
+        );
+
         txtview_btnA = (TextView) findViewById(R.id.txtButtonA);
         imgbtn_btnA = (ImageButton) findViewById(R.id.imgButtonA);
+        layout = (LinearLayout) findViewById(R.id.linearlayout);
 
-        imgbtn_btnA.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                txtview_btnA.setAllCaps(true);
-            }
-        });
+        layout.addView(sbutton.CreateImageButton());
     }
 }
--- app/src/main/java/com/buttons/lacueva/krakosky/lacuevabuttons/SoundButton.java
+++ app/src/main/java/com/buttons/lacueva/krakosky/lacuevabuttons/SoundButton.java
@@ -1,15 +1,62 @@
 package com.buttons.lacueva.krakosky.lacuevabuttons;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class SoundButton {
 
-    private String name;
-    private String idClip;
-
-    public SoundButton(String btnName, String resourceId) {
-        this.name = btnName;
-        this.idClip = resourceId;
+    public enum Color {
+        BLUE
     }
 
+    private Context context;
+    private String name;
+    private int idAudioResource;
+    private Color color;
+
+    public SoundButton(Context context, String btnName, int resourceId, Color color) {
+        this.context = context;
+        this.name = btnName;
+        this.idAudioResource = resourceId;
+        this.color = color;
+    }
+
+
+    public void push()
+    {
+
+    }
+
+    public TextView CreateTextView(SoundButton button)
+    {
+        TextView txtview = new TextView(context);
+        txtview.setText(button.getName());
+
+        return txtview;
+    }
+
+    public ImageButton CreateImageButton()
+    {
+        final ImageButton imgbtn = new ImageButton(context);
+        imgbtn.setId(name.hashCode());
+        imgbtn.setImageDrawable(R.drawable.ic_launcher_background);
+        imgbtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgbtn.setPressed(true);
+                Toast.makeText(context, "Bing!", Toast.LENGTH_SHORT);
+            }
+        });
+
+        return imgbtn;
+    }
+
+    /************************
+     * Getters & Setters
+     ************************/
 
     public String getName() {
         return name;
@@ -19,11 +66,4 @@ public class SoundButton {
         this.name = name;
     }
 
-    public String getIdClip() {
-        return idClip;
-    }
-
-    public void setIdClip(String idClip) {
-        this.idClip = idClip;
-    }
 }
