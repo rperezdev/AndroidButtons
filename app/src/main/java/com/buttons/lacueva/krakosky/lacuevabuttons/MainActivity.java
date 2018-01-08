2ee1f6410c01d7efbf65d63fa3244cf13fd6d7b0
krakosky <rperez.dev@gmail.com>
2018-01-09 00:45:53 +0100

    Now sound can be playe

--- app/src/main/java/com/buttons/lacueva/krakosky/lacuevabuttons/MainActivity.java
+++ app/src/main/java/com/buttons/lacueva/krakosky/lacuevabuttons/MainActivity.java
@@ -2,7 +2,11 @@ package com.buttons.lacueva.krakosky.lacuevabuttons;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
@@ -15,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -44,64 +49,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
+        intent.setType("audio/*");
         startActivityForResult(intent, REQUEST_CODE_ADD_AUDIO);
-
-        /*
-        SoundButton sbutton = new SoundButton(
-                MainActivity.this,
-                "Sample",
-                R.raw.sample,
-                SoundButton.Color.BLUE
-        );
-
-        txtview_btnA = (TextView) findViewById(R.id.);
-        imgbtn_btnA = (ImageButton) findViewById(R.id.imgButtonA);
-        layout = (LinearLayout) findViewById(R.id.linearlayout);
-
-        layout.addView(sbutton.CreateImageButton());
-        */
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         if(requestCode == REQUEST_CODE_ADD_AUDIO && resultCode == Activity.RESULT_OK) {
-            /*
-            try {
-                MemoryManager.addAudioToFolder(new java.io.File(data));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            */
 
-            File f = new File("adf");
-
-            Toast.makeText(this, data.getData() + " " + f.exists(), Toast.LENGTH_LONG).show();
-
-            /*
-            File f = new File(Environment.getExternalStorageDirectory() + "/LaCuevaButtons", ""+data.hashCode());
+            InputStream is = null;
 
             try {
-                InputStream is = getContentResolver().openInputStream(data.getData());
-                OutputStream os = new FileOutputStream(f);
-
-                byte[] buffer = new byte[1024];
-
-                int len = 0;
-                while((len = is.read(buffer)) > 0) {
-                    os.write(buffer);
-                }
-
+                is = getContentResolver().openInputStream(data.getData());
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
+            }
+
+            MediaPlayer mp = new MediaPlayer();
+
+            FileInputStream fis = (FileInputStream)is;
+
+            try {
+                mp.setDataSource(fis.getFD());
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-*/
+            try {
+                mp.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-
+            mp.start();
         }
     }
 }
