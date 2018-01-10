a559754970a5b734d98b73b42e7542fc4878da96
rperezdev <rperez.dev@gmail.com>
2018-01-10 01:08:51 +0100

    Now buttons can be created

--- app/src/main/java/com/buttons/lacueva/krakosky/lacuevabuttons/MainActivity.java
+++ app/src/main/java/com/buttons/lacueva/krakosky/lacuevabuttons/MainActivity.java
@@ -1,45 +1,48 @@
 package com.buttons.lacueva.krakosky.lacuevabuttons;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.database.Cursor;
+import android.graphics.Color;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_ADD_AUDIO = 1;
 
-    private TextView txtview_btnA;
-    private ImageButton imgbtn_btnA;
-
     private LinearLayout layout;
+    private ImageButton button;
+
+    private InputStream is;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = (LinearLayout) findViewById(R.id.linear_layout);
+        button = (ImageButton) findViewById(R.id.imageButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                triggerFilePicker();
+            }
+        });
+
+        is = null;
 
         String rootDirectory = Environment.getExternalStorageDirectory().toString();
 
@@ -48,9 +51,7 @@ public class MainActivity extends AppCompatActivity {
             myDir.mkdirs();
         }
 
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("audio/*");
-        startActivityForResult(intent, REQUEST_CODE_ADD_AUDIO);
+        //triggerFilePicker();
     }
 
     @Override
@@ -58,31 +59,62 @@ public class MainActivity extends AppCompatActivity {
     {
         if(requestCode == REQUEST_CODE_ADD_AUDIO && resultCode == Activity.RESULT_OK) {
 
-            InputStream is = null;
-
             try {
                 is = getContentResolver().openInputStream(data.getData());
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
 
-            MediaPlayer mp = new MediaPlayer();
+            layout.addView(getImageButton(is));
 
-            FileInputStream fis = (FileInputStream)is;
-
-            try {
-                mp.setDataSource(fis.getFD());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                mp.prepare();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            mp.start();
+            Toast.makeText(this, "Sound added", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public ImageButton getImageButton(final InputStream istream)
+    {
+        ImageButton ibutton = new ImageButton(this);
+        ibutton.setId(ibutton.hashCode());
+        ibutton.setAlpha(0.8f);
+        ibutton.setBackgroundColor(Color.TRANSPARENT);
+        ibutton.setImageResource(R.drawable.ic_launcher_background);
+        ibutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PlayAudio(istream);
+            }
+        });
+
+        return ibutton;
+    }
+
+    public void triggerFilePicker() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
+        startActivityForResult(intent, REQUEST_CODE_ADD_AUDIO);
+    }
+
+    public static void PlayAudio(InputStream is)
+    {
+        if(is == null)
+            return;
+
+        MediaPlayer mp = new MediaPlayer();
+
+        FileInputStream fis = (FileInputStream)is;
+
+        try {
+            mp.setDataSource(fis.getFD());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mp.start();
     }
 }
